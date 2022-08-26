8c3d437f5e412f6971c50032661327a1eade2c8d
TiagoM13 <thyagomotha2000@gmail.com>
2022-08-25 23:04:43 -0300

    adding styles global

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from "next/head";
 
-import styles from "../styles/home.module.scss";
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Ínicio | ig.news</title>
       </Head>
-      <h1 className={styles.title}>
+      <h1>
         Hello World
       </h1>
     </>
