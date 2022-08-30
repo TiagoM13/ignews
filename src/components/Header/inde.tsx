b3854d1e9b8cf993832001fd1ba3fbf6e228d273
TiagoM13 <thyagomotha2000@gmail.com>
2022-08-29 23:47:49 -0300

    add component header

---
+++ src/components/Header/inde.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Image from 'next/image';
+// import images
+import logo from '../../../public/images/logo.svg';
+// import styles
+import styles from './styles.module.scss';
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.headerContainer}>
+      <div className={styles.headerContent}>
+        <Image src={logo} alt="ig.news" />
+        <nav>
+          <a className={styles.active} href="#">Home</a>
+          <a href="#">Post</a>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
