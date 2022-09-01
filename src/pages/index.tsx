f526d9baebae428eff928f2aa9c3c28eaac55e9b
TiagoM13 <thyagomotha2000@gmail.com>
2022-08-31 21:29:51 -0300

    feat: adding stripe services and SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetServerSideProps } from 'next'
 import Head from 'next/head';
 import Image from 'next/image';
 
@@ -5,8 +6,16 @@ import styles from './home.module.scss';
 
 import Img from '../../public/images/avatar.svg';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,14 +27,32 @@ export default function Home() {
           <h1>News about the <span>React</span> world</h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 months</span>
+            <span>for {product.amount} months</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <Image src={Img} alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Ld0M2LwO9bfpa9MytPqS4jS')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    }
+  }
 }
