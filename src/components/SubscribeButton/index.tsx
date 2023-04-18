c891e966e2cfdce51d7426aa580f46c28b079bc3
TiagoM13 <thyagomotha2000@gmail.com>
2023-04-18 14:09:44 -0300

    add props to subscribe button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import styles from "./styles.module.scss";
 
-// interface SubscribeButtonProps {
-//   priceId: string;
-// }
+interface SubscribeButtonProps {
+  priceId: string;
+}
 
-export function SubscribeButton() {
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   return (
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
