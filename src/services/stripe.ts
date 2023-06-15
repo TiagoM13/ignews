c5779d72244712c362270310a80d15040e81521c
TiagoM13 <thyagomotha2000@gmail.com>
2023-06-15 12:38:31 -0300

    add version package to stripe file

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,10 +1,9 @@
 import Stripe from "stripe";
-import { version } from "../../package.json";
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: "2022-11-15",
   appInfo: {
     name: "ignews",
-    version,
+    version: "0.1.0",
   },
 });
