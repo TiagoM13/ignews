ed6814de356daf04e723228ec02d8b170e75a891
TiagoM13 <thyagomotha2000@gmail.com>
2023-06-15 21:32:58 -0300

    fixing stripe version

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,9 +1,10 @@
 import Stripe from "stripe";
+import { version } from "../../package.json";
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: "2022-11-15",
   appInfo: {
     name: "ignews",
-    version: "0.1.0",
+    version: version,
   },
 });
