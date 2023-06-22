bb7a928fd81b9eb44b9efe7eb282f90a6a09ee5f
TiagoM13 <thyagomotha2000@gmail.com>
2023-06-21 22:09:34 -0300

    adding static props

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,4 +1,10 @@
+/* eslint-disable @next/next/no-typos */
 import Head from 'next/head';
+import { GetStaticProps } from 'next';
+import Prismic from '@prismicio/client';
+
+import { getPrismicClient } from '../../services/prismic';
+
 import styles from './styles.module.scss';
 
 export default function Posts() {
@@ -29,4 +35,21 @@ export default function Posts() {
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication')
+  ], {
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100,
+  })
+
+  console.log(JSON.stringify(response, null, 2))
+
+  return {
+    props: {}
+  }
 }
